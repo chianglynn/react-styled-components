37ab53d938decd803770a086d1a205a0af5d493f
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-13 12:32:51 +0800

    Revert "Upload starter files"
    This reverts commit 6f200bff684a4d5c3571136dd3cc147778a231f7.

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Styled Components </h1>
-    </div>
-  )
-}
-
-export default App
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export const colors = {
-  primary: '#645cff',
-}
-
-export const setupBorder = ({ width, type = 'solid', color }) => {
-  return `${width}px ${type} ${color}`
-}
