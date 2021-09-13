9fa95e153c930a58539fd85e6a7e37abf14604b7
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-13 14:56:42 +0800

    Extend styled-component

--- styled/src/App.js
+++ styled/src/App.js
@@ -1,13 +1,15 @@
 import BasicTitle from './components/BasicTitle';
-import { DefaultButton } from './components/Buttons';
+import { DefaultButton, HipsterButton } from './components/Buttons';
 
 function App() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <BasicTitle>styled components</BasicTitle>
-      <DefaultButton>click me</DefaultButton>
-    </div>
-  );
+    return (
+        <div style={{ padding: '2rem' }}>
+            <BasicTitle special>styled components</BasicTitle>
+            <BasicTitle>styled components</BasicTitle>
+            <DefaultButton>click me</DefaultButton>
+            <HipsterButton>click me</HipsterButton>
+        </div>
+    );
 }
 
 export default App;
--- styled/src/components/Buttons.js
+++ styled/src/components/Buttons.js
@@ -1,14 +1,21 @@
 import styled from 'styled-components';
 
 export const DefaultButton = styled.button`
-  display: block;
-  padding: .25rem;
-  margin: 1rem auto;
-  width: 200px;
-  color: #fff;
-  text-transform: capitalize;
-  border: none;
-  border-radius: .25rem;
-  background-color: #645cff;
-  cursor: pointer;
+    display: block;
+    padding: .25rem;
+    margin: 1rem auto;
+    width: 200px;
+    color: #fff;
+    text-transform: capitalize;
+    border: none;
+    border-radius: .25rem;
+    background-color: #645cff;
+    cursor: pointer;
+`;
+
+export const HipsterButton = styled(DefaultButton)`
+    width: 400px;
+    color: #645cff;
+    border: 1px solid #645cff;
+    background-color: transparent;
 `;
